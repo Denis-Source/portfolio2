6b1e554d24fcb6333886f5b5a930b1c6476ebe52
Denis <denis.zol@code-care.pro@gmail.com>
2022-08-23 01:08:43 +0300

    add proper seo tags

--- frondend/components/Head/Head.tsx
+++ frondend/components/Head/Head.tsx
@@ -1,12 +1,19 @@
 import Head from "next/head";
 import {HeadModel} from "./Head.model";
+import getUrl from "../../api";
 
 const HeadComponent = ({head}: { head: HeadModel }) => {
     return (
         <Head>
             <title>{head.title}</title>
-            <meta itemProp="description" content={head.description}/>
-            <meta itemProp="image" content={head.share_image}/>
+            <meta name="description" content={head.description}/>
+            <meta property="og:url"
+                  content="https://new.zoloto.cx.ua"/>
+            <meta property="og:type" content="article"/>
+            <meta property="og:title" content={head.title}/>
+            <meta property="og:description" content={head.description}/>
+            <meta property="og:image"
+                  content={getUrl(head.share_image)}/>
         </Head>
     );
 };
